51404fbb6ed2316d062603ae667a4ea7be36a59b
ambeech1 <amb549@pitt.edu>
2025-02-17 19:07:20 -0500

    Adding SymPy tests.

--- Tests/Test_Potential_Read.py
+++ Tests/Test_Potential_Read.py
@@ -4,18 +4,28 @@ import matplotlib.pyplot as plt
 
 from Solvers.SolverTISE import SolverTISE
 
-X = sp.symbols("x")
-A = sp.symbols("a")
-H = sp.symbols("h")
+x = sp.symbols("x")
+a = sp.symbols("a")
+h = sp.symbols("h")
 
-W = sp.exp(A * (X - H) ** (-2))
+u = sp.Function('u')
+
+#class u(sp.Function):
+    #@classmethod
+    #def eval(cls, x):
+        #a = np.ones_like(x)
+        #for i in range(len(a)):
+            #if x[i] < 0:
+                #a[i] = 0
+        #return a
 
 solver = SolverTISE()
 string = input("Enter a potential: ")
-try:
-    expr = sp.sympify(string)
-    U = sp.lambdify(X, expr, "numpy")
-    plt.plot(solver.x, U(solver.x))
-    plt.show()
-except:
-    print("ERROR")
+    #try:
+expr = sp.sympify(string)
+expr = expr.subs(u(x), sp.Heaviside(x))
+U = sp.lambdify(x, expr, "numpy")
+plt.plot(solver.x, U(solver.x))
+plt.show()
+    #except:
+        #print("ERROR")
